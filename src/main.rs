fd79d192ec925b6039d6f1c4df43dfeb56a717bd
Jack Jennings <jack@standard-library.com>
2018-06-22 10:56:47 -0700

    Add HTML template

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,37 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+extern crate rocket_contrib;
 
-use rocket::response::NamedFile;
 use rocket::request::Request;
+use rocket::response::NamedFile;
+use rocket_contrib::Template;
+use std::collections::HashMap;
+use std::io::Read;
 use std::path::{Path, PathBuf};
 
+fn render(filepath: PathBuf) -> Option<Template> {
+    let file = NamedFile::open(filepath);
+
+    file.map(|mut f| {
+        let mut context = HashMap::new();
+        let mut content = String::new();
+
+        f.read_to_string(&mut content).ok();
+        context.insert("content", content);
+
+        Template::render("page", context)
+    }).ok()
+}
+
 #[get("/<path..>")]
-fn page(path: PathBuf) -> Option<NamedFile> {
-    let filepath = Path::new("test/pages/").join(path).join("index.txt");
-    NamedFile::open(filepath).ok()
+fn page(path: PathBuf) -> Option<Template> {
+    render(Path::new("test/pages/").join(path).join("index.txt"))
 }
 
 #[get("/")]
-fn index() -> Option<NamedFile> {
-    let filepath = Path::new("test/pages/").join("index.txt");
-    NamedFile::open(filepath).ok()
+fn index() -> Option<Template> {
+    render(Path::new("test/pages/").join("index.txt"))
 }
 
 #[error(404)]
@@ -28,5 +44,6 @@ fn main() {
     rocket::ignite()
         .mount("/", routes![page, index])
         .catch(errors![not_found])
+        .attach(Template::fairing())
         .launch();
 }
